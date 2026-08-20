a9f409b581b662fcfe32e7c785e4ec8b68e0d4f0
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-08-20 11:58:40 +0330

    Add ContactUs model

---
+++ Home/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Contact_us
+
+
+
+class ContactUs(forms.ModelForm):
+    
+    class Meta :
+        
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class ContactUs(models.Model):
+    
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    
+    def __str__(self):
+        return f"{self.name} - {self.text[:30]}"
