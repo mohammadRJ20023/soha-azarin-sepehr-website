9d9e3466b495e0a03bae7a78d4fbd71f36c257a3
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-08-23 01:10:51 +0330

    fix(ContactUs): improve ContactUs model and form validation

--- Home/forms.py
+++ Home/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import ContactUs
-
+from django.core.validators import ValidationError
+import re
 
 
 class ContactUsForm(forms.ModelForm):
@@ -14,14 +15,21 @@ class ContactUsForm(forms.ModelForm):
                 "placeholder" : "نام شما...",
                 "autocomplete" : "on"
             }),
+            "phone_number":forms.TextInput(attrs={
+                "class" : "col-lg-12",
+                "placeholder" : "شماره تلفن شما..."
+            }),
             "email": forms.EmailInput(attrs={
                 "class" : "col-lg-12",
                 "placeholder" : "ایمیل شما..."
-                
             }),
             "text" : forms.TextInput(attrs={
                 "class" : "col-lg-12",
                 "placeholder" : "پیام یا درخواست خود را بنویسید..."
             })
         }
-        
+    def clean_phone_number(self):
+        phone = self.cleaned_data.get("phone_number")
+        if not re.match(r'^09\d{9}$', phone):
+            raise ValidationError("شماره تلفن باید با ۰۹ شروع شود و ۱۱ رقم باشد.", code="invalid_number")
+        return phone
--- Home/models.py
+++ Home/models.py
@@ -7,6 +7,7 @@ class ContactUs(models.Model):
     
     name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
+    phone_number = models.CharField(max_length=11, blank=False, null=False)
     text = models.TextField(null=False, blank=False)
     
     def __str__(self):
