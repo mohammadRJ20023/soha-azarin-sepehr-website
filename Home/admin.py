cf870130ee983f4d6f5b24f85360e0d9a0961fb6
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-08-24 01:58:58 +0330

    feat(Home): add TeamMember model for team section

--- Home/admin.py
+++ Home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ContactUs,CompanyStats
+from .models import ContactUs,CompanyStats, TeamMember
 
 admin.site.register(ContactUs)
 admin.site.register(CompanyStats)
+admin.site.register(TeamMember)
--- Home/models.py
+++ Home/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from portfolio.models import Project
 
-
-
+class TeamMember(models.Model):
+    
+    full_name = models.CharField(max_length=200, verbose_name="نام و نام خانوادگی")
+    job  = models.CharField(max_length=100, verbose_name="سمت")
+    image = models.ImageField(upload_to="images/team", verbose_name="تصویر ")
+    
+    inestagram = models.URLField(blank=True, verbose_name="اینستاگرام")
+    linkedin_url = models.URLField(blank=True, verbose_name="لینکدین")
+    twitter_url = models.URLField(blank=True, verbose_name="توییتر")
+    
+    #class Meta:
+        #verbose_name = "اعضای تیم"
+    
+    def __str__(self):
+        return self.full_name
 
 class ContactUs(models.Model):
     
