b4e10168492fd5e48cf52bb92ff49b73b70e9877
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-08-23 12:35:07 +0330

    feat(portfolio): add project list page with pagination and styling

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "portfolio"
 
 urlpatterns = [
-    path("detail/<str:slug>", views.Project_detail, name="project_detail")
+    path("detail/<str:slug>", views.Project_detail, name="project_detail"),
+    path("list", views.ProjectList.as_view(), name="project_list")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
+from django.views.generic import ListView
 
 
 
@@ -12,3 +13,12 @@ def Project_detail(request, slug):
     
 
     return render(request, "portfolio/Project_detail.html", {"project": project})
+
+
+class ProjectList(ListView):
+    
+    model = Project
+    template_name = "portfolio/Project_list.html"
+    paginate_by = 2 
+    context_object_name = "project_list"
+    queryset = Project.objects.all()
